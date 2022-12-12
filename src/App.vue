af357f05d8e0d1c8cb8d6ee799f2d753de4f9661
jisukim92 <jisu.kim92@hancommds.com>
2022-12-12 16:02:30 +0900

    첫 번째 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>안녕하세여</h2>
+  <div v-for="(i,x) in products" :key="x">
+    <h4>{{i.name}} </h4>
+    <p>{{i.price}} 만원</p>
+    
+  </div>
+    <p>{{x}}인덱스값</p>
+    <p v-if="x < 10"> 끝입니다. </p>
+    <p v-else> 더보기 .</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      //데이터 보관함이라 생각하면됨, 
+      products : [
+         { name : '1번원룸'  , price : 600}, 
+         { name : '2번투룸'  , price : 500}, 
+         { name : '3번쓰리룸'  , price : 400}
+    
+      ],
+
+
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
