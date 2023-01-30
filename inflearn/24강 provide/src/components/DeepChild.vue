7d49978e9f7c8e830d2058559ac888096bf5e62c
niagarafalls729 <wltn729@gmail.com>
2023-01-30 23:15:18 +0900

    생명주기 진행중

---
+++ inflearn/24강 provide/src/components/DeepChild.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <div class="card-header">Deep Card Header</div>
+    <div class="card-body">
+      <p>sMSG1 ::: {{ staticMSG }},</p>
+      <p>MSG : : : {{ MSG }}</p>
+      <p>count ::: {{ count }}</p>
+      <input v-model="newDeepValue" type="text" />
+      <button @click="fnUpdateMSG">fnUpdateMSG</button>
+
+    </div>
+  </div>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import { inject } from 'vue';
+import { ref } from 'vue';
+export default {
+  setup() {
+    // const { staticMSG ,} = inject('provide-key-staticMSG');
+    // 기본적으로 구조 분해로 할당받을때는 key 값을 동일시하자.
+    const { staticMSG, updateStaticMSG } = inject('provide-key-staticMSG');
+    const fnUpdateMSG = () => {
+      updateStaticMSG(newDeepValue.value);
+    };
+    const MSG = inject('provide-key-MSG');
+    // 이렇게 직접하지말고 부모컴포넌트에서 하는게 좋다.
+    // MSG.value = MSG.value + '11';
+    const newDeepValue = ref('');
+    const count = inject('provide-key-count');
+    const myVal = inject('myVal-key');
+    return { myVal, fnUpdateMSG, newDeepValue, MSG, count, staticMSG };
+  },
+};
+</script>
---
+++ inflearn/실행용 모듈/vue-start-module/src/components/LifecycledHooks.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container py-4">
+    <input type="text" ref="inputTAG" value="hello!!!" />
+    <LifecycleChild></LifecycleChild>
+    <p>{{ CNT }}</p>
+    <button @click="CNT++;">CNT++</button>
+    <!-- <p ref="MSG" v-bind:value="MSG"></p> -->
+  </div>
+</template>
+
+<script>
+import { onBeforeMount, onBeforeUpdate, onMounted, onUpdated, ref } from 'vue';
+import LifecycleChild from './LifecycleChild.vue';
+export default {
+  components: { LifecycleChild },
+  setup() {
+    console.log('1 setup');
+    const inputTAG = ref(null);
+    const CNT = ref(1);
+    const MSG = ref('');
+    onBeforeMount(() => {
+      console.log('4 onBeforeMount' + inputTAG.value);
+    });
+    onMounted(() => {
+      console.log('5 onMounted' + JSON.stringify(inputTAG.value._value));
+    });
+    onBeforeUpdate(() => {
+      console.log('  onBeforeUpdate');
+    });
+    onUpdated(() => {
+      console.log('  onUpdated');
+    });
+    return { inputTAG, CNT, MSG };
+  },
+  beforeCreate() {
+    console.log('2 beforeCreate');
+  },
+  created() {
+    console.log('3 created');
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
